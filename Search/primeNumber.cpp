6ce06f00299a1729e53efbf1cf0f82db82770b7a
thelittleho <jyh20041206@gmail.com>
2024-07-19 22:23:09 +0900

    에라토스테네스의 체

---
+++ Search/primeNumber.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int number = 120;
+int prime[121];
+
+void primeNumber()
+{
+    for (int i = 2; i < number; i++)
+    {
+        prime[i] = i;
+    }
+    
+    for (int i = 2; i <= sqrt(number); i++)
+    {
+        if (prime[i] == 0)
+        {
+            continue;
+        }
+        for (int j = i*i; j <= number; j++)
+        {
+            prime[j] = 0;
+        }
+    }
+
+    for (int i = 2; i < number; i++)
+    {
+        if (prime[i] != 0)
+        {
+            cout << prime[i] << " ";
+        }
+        
+    }
+    
+    
+}
